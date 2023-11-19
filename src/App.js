6ff927eed1a47d3dd4fc4b3c3c11b50dbef6eaf5
avinasdube <avidubey712@gamil.com>
2023-11-19 23:17:34 +0530

    [loading-screen] : added new loading screen component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import './App.css';
+import { useEffect, useState } from 'react';
+import './App.scss';
+import LoadingScreen from './components/Logo/LoadingScreen';
+
+import dev from './assets/dev.png';
 
 function App() {
+
+  // CODE TO IMPLEMENT LOADING SCREEN 
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   return (
-    <div className="App">
-      ListDown
-    </div>
+    <>
+      {loading === true ? <LoadingScreen /> :
+        <div className="App">
+          <img src={dev} alt=''></img>
+          <span>Under Construction. Come back shortly.</span>
+        </div>
+      }
+    </>
   );
 }
 
