4bc5c313f9c7945bd3327925cf1ff0310b855415
avinasdube <avidubey712@gamil.com>
2023-11-20 21:37:25 +0530

    [component] : created new 'sidebar' component

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import './Sidebar.scss';
+
+import all from '../../assets/all.png';
+import today from '../../assets/today.png';
+import upcoming from '../../assets/upcoming.png';
+
+const Sidebar = () => {
+  const [section, setSection] = useState('at');
+
+  const sections = [
+    {
+      id: 1,
+      title: 'All Tasks',
+      img: all,
+      value: 'at',
+    },
+    {
+      id: 2,
+      title: 'Today',
+      img: today,
+      value: 'td',
+    },
+    {
+      id: 3,
+      title: 'Upcoming',
+      img: upcoming,
+      value: 'uc'
+    }
+  ]
+
+  return (
+    <div className="sidebarContainer">
+      <div className="options">
+        {sections.map((secton) => (
+          <button className={`${secton.value === section ? 'active' : ''}`} value={secton.value} onClick={(e) => setSection(e.target.value)} key={secton.id}>
+            <img src={secton.img} alt=''></img>{secton.title}
+          </button>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
