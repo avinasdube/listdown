5fa6bdcab7939dcdd0c5bb603ac9e3cff2dc8095
avinasdube <avidubey712@gamil.com>
2023-11-21 16:49:05 +0530

    [modified] : modified layout, ui, color schemes etc.

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -2,65 +2,38 @@ import React from 'react';
 import './TaskList.scss';
 
 import edit from '../../assets/edit.png';
-import remove from '../../assets/delete.png'
+import remove from '../../assets/delete.png';
+import notask from '../../assets/notask.png';
+import { useDispatch } from 'react-redux';
+import { checkTodo, removeTodo } from '../../reducers/todoReducers';
 
-const TaskList = () => {
-    const tasklist = [
-        {
-            id: 1,
-            text: 'Write Assignment',
-            label: 'Today',
-        },
-        {
-            id: 2,
-            text: 'Go to market',
-            label: 'Upcoming',
-        },
-        {
-            id: 3,
-            text: 'Read documentation',
-            label: 'Today',
-        },
-        {
-            id: 4,
-            text: 'Wash Clothes',
-            label: 'Today',
-        },
-        {
-            id: 5,
-            text: 'Arrange documents',
-            label: 'Upcoming',
-        },
-        {
-            id: 6,
-            text: 'Write poetry',
-            label: 'Upcoming',
-        },
-        {
-            id: 7,
-            text: 'Learn about CI/CD workflows',
-            label: 'Today',
-        }
-    ]
+const TaskList = ({ tasklist }) => {
+
+    const dispatch = useDispatch();
 
     return (
         <div className="listContainer">
-            {tasklist.map((task) => (
-                <div className="taskBox" key={task.id}>
-                    <div className="content">
-                        <input type='checkbox'></input>
-                        <div className="details">
-                            <span>{task.text}</span>
-                            {task.label === 'Today' ?
-                            <span className='label' style={{color: 'green'}}>{task.label}</span> :
-                            <span className='label' style={{color: 'firebrick'}}>{task.label}</span>}
+            {tasklist.length === 0 ?
+                <div className='notasks'>
+                  <img src={notask} alt=''></img>
+                  <span>No tasks found ! Start by creating a new one.</span>
+                </div> :
+                tasklist.map((task, index) => (
+                    <div className="taskBox" key={task.id}>
+                        <div className="content">
+                            <input type='checkbox' id={index} checked={task.checked} onChange={(e) => dispatch(checkTodo(e.target.id))}></input>
+                            <div className="details">
+                                <span>{task.text}</span>
+                                {task.label === 'Today' ?
+                                    <span className='label' style={{ color: 'green' }}>{task.label}</span> :
+                                    <span className='label' style={{ color: 'firebrick' }}>{task.label}</span>}
+                            </div>
                         </div>
-                    </div>
-                    <div className="action">
-                        <img src={edit} alt=''></img>
-                        <img src={remove} alt=''></img>
-                    </div>
-                </div>))}
+                        <div className="action">
+                            <img src={edit} alt=''></img>
+                            <img src={remove} alt='' onClick={() => dispatch(removeTodo(task.id))}></img>
+                        </div>
+                    </div>))}
         </div>
     )
 }
